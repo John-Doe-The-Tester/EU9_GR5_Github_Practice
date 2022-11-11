5fbc2aca9a4e78e5cea0c086d0727e64ace63c31
TandoganErten <tereten0706@gmail.com>
2022-11-11 21:51:03 +0300

    first commit for demo

---
+++ src/test/java/com/eu9gr5/step_definitions/VehicleArrangementTableDef.java
@@ -0,0 +1,10 @@
+package com.eu9gr5.step_definitions;
+
+import org.junit.Test;
+
+public class VehicleArrangementTableDef {
+    @Test
+    public void test1(){
+        System.out.println("mayday mayday");
+    }
+}
